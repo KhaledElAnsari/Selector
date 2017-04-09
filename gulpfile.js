36cf260a42896d9df0059b8beb0141a27e7fc7ef
Mark Entingh <Mark Entingh>
2017-04-09 14:01:18 -0400

    added selector.js to project from github.com/websilk/home

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//includes
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify')
+
+//tasks
+gulp.task('selector', function () {
+    return gulp.src('selector.js', { base: '.' })
+        .pipe(concat('dist/selector.js'))
+        .pipe(gulp.dest('.', { overwrite: true }));
+});
+
+gulp.task('selector-min', function () {
+    return gulp.src('selector.js', { base: '.' })
+        .pipe(concat('dist/selector.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('.', { overwrite: true }));
+});
